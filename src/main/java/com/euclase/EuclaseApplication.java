0e731419aef715fc51f581d670811c28d1d9435b
nuochka <anstskovalenko@gmail.com>
2024-06-19 20:17:05 +0200

    Added initializing project files

---
+++ src/main/java/com/euclase/EuclaseApplication.java
@@ -0,0 +1,13 @@
+package com.euclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuclaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuclaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/euclase/EuclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuclaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
