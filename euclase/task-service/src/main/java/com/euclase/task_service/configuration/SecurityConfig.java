fd9c1ca296bfd468700d423ae3c0031bc743b3c4
nuochka <anstskovalenko@gmail.com>
2024-07-03 23:25:05 +0200

    Added security configuration for fixing redirect to sign in page

---
+++ euclase/task-service/src/main/java/com/euclase/task_service/configuration/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.euclase.task_service.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/tasks").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
